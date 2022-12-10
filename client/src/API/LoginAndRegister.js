1ed731bdf427337b74d1652c14b1ce9ae6452797
MeirDi <321mid@gmail.com>
2022-12-11 00:02:46 +0200

    Add Material UI to project and changing design in login page

--- client/src/API/LoginAndRegister.js
+++ client/src/API/LoginAndRegister.js
@@ -1,20 +1,13 @@
 import axios from "axios";
 
 export async function login(identifier, password) {
-  return await axios
-    .post("/api/auth/local", {
+  try {
+    const response = await axios.post("/api/auth/local", {
       identifier,
       password,
-    })
-    .then((response) => {
-      // If request is not successful, display error message
-      if (!response.status === 200) {
-        throw new Error("HTTP status " + response.status);
-      }
-
-      return response.data;
-    })
-    .catch((err) => {
-      console.log(err);
     });
+    return response;
+  } catch (error) {
+    return error.response.data.error.message;
+  }
 }
--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,70 +1,104 @@
 import React from "react";
 import { login } from "../API/LoginAndRegister.js";
-import { Container, Form, Button, Alert, Card } from "react-bootstrap";
 import { useState } from "react";
+import { Box, Button, TextField, Typography, Alert } from "@mui/material";
 
 export default function Login({ onLoginSuccessful }) {
-  const [email, setEmail] = useState("");
+  const [alert, setAlert] = useState(undefined);
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [hasError, setHasError] = useState(false);
 
-  const onEmailChange = (event) => setEmail(event.target.value);
+  const showAlert = (alert) => {
+    setAlert(alert);
+    setTimeout(() => {
+      setAlert(undefined);
+    }, 5000);
+  };
+
+  const onUserNameChange = (event) => setUserName(event.target.value);
   const onPasswordChange = (event) => setPassword(event.target.value);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    setHasError(false);
-    const loginResult = await login(email, password);
-    if (!loginResult) setHasError(true);
-    else {
-      const { user, jwt } = loginResult;
-      // Save user IDs on local storage
-      localStorage.setItem("name", user.username);
-      localStorage.setItem("token", jwt);
-      onLoginSuccessful();
+
+    if (userName && password) {
+      const loginResult = await login(userName, password);
+      if (loginResult.data) {
+        const { user, jwt } = loginResult.data;
+        // Save user IDs on local storage
+        localStorage.setItem("name", user.username);
+        localStorage.setItem("token", jwt);
+        onLoginSuccessful();
+      } else {
+        showAlert({ message: loginResult });
+      }
+    } else {
+      return;
     }
   };
 
   return (
-    <Container>
-      <Card className="mt-5">
-        <Card.Header as="h1">Login</Card.Header>
-        <Card.Body>
-          <Form className="w-100" onSubmit={onSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>User Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter email"
-                onChange={onEmailChange}
-                value={email}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={onPasswordChange}
-                value={password}
-              />
-            </Form.Group>
-            {hasError && (
-              <Alert variant={"danger"}>
-                The email address and password you entered don't match any
-                account. Please try again.
-              </Alert>
-            )}
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-    </Container>
+    <div>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+          bgcolor: "background.paper",
+          boxShadow: 1,
+          borderRadius: 2,
+          p: 2,
+          width: 300,
+          m: "auto",
+          mt: "10%",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography
+          sx={{ mb: 2, textDecoration: "underline" }}
+          variant="h5"
+          component="div"
+        >
+          Login
+        </Typography>
+        <TextField
+          id="outlined-username-input"
+          label="User Name"
+          type="text"
+          inputProps={{
+            form: {
+              autocomplete: "off",
+            },
+          }}
+          onChange={onUserNameChange}
+          value={userName}
+          required={true}
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          onChange={onPasswordChange}
+          value={password}
+          required={true}
+        />
+        <Typography component="div">
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            className="form__custom-button"
+            onClick={onSubmit}
+          >
+            Log in
+          </Button>
+        </Typography>
+        {alert && (
+          <Alert severity="error" sx={{ mt: 1 }}>
+            {alert.message}
+          </Alert>
+        )}
+      </Box>
+    </div>
   );
 }
